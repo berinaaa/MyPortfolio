aaebdbf33c25ea4ca0bf1a7ee562236d255b516a
Berina <berina@Berinas-MacBook-Air.local>
2023-07-20 15:32:22 +0200

    first commit

--- app.js
+++ app.js
@@ -8,21 +8,15 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
-    // document.querySelector(".theme-btn").addEventListener("click", () => {
-    //     document.body.classList.toggle("light-mode");
-    // })
     document.querySelector('.send_email').addEventListener("click", (e) => {
         e.preventDefault();
         console.log(document.querySelector('.contact-form'))
         emailjs.sendForm(
             'service_j9eatkd','template_afnpx34', document.querySelector('.contact-form'))
             .then((result) => {
-                // window.alert("The email has been successfuly sent") simple
-                // setMessage("Your email has been sucessfully sent");
                 console.log(result)
             }, (error) => {
                 console.log(error)
-                // setMessage("Something went wrong! Please try again!");
             });
     })
     
